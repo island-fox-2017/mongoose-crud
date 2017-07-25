6dcffa477a0a4addd5037899971e8f164a71f4fd
adeanugerah89 <ade_anugerahtb@yahoo.com>
2017-07-25 15:13:38 +0700

    CRUD books done

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -24,7 +24,33 @@ var getAllBook = (req,res) => {
   })
 }
 
+var updateBook = (req,res) => {
+  Books.update({ _id: req.params.id }, { $set: req.body}, (err,result) => {
+    if(err) res.send(err)
+    console.log(result);
+    res.send(result)
+  })
+}
+
+var deleteBook = (req,res) => {
+  Books.findByIdAndRemove(req.params.id, (err) => {
+    if(err) res.send(err)
+    res.send('data already delete')
+  })
+}
+
+var findOneBook = (req,res) => {
+  Books.findById(req.params.id,(err,result) => {
+    if(err) res.send(err)
+    console.log(result);
+    res.send(result);
+  })
+}
+
 module.exports = {
   createBook,
-  getAllBook
+  getAllBook,
+  updateBook,
+  deleteBook,
+  findOneBook
 }
--- routes/books.js
+++ routes/books.js
@@ -6,5 +6,8 @@ var controller = require('../controllers/bookController');
 /* GET users listing. */
 router.post('/', controller.createBook);
 router.get('/', controller.getAllBook);
+router.put('/:id', controller.updateBook);
+router.delete('/:id', controller.deleteBook);
+router.get('/:id', controller.findOneBook);
 
 module.exports = router;
